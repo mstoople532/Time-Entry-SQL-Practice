46c99e5d75a064650c917746579ad05bce500ecb
Molly <mstoople@gmail.com>
2017-02-28 15:45:23 -0500

    Completed successful test run of timestamps with client names

--- app.rb
+++ app.rb
@@ -23,4 +23,10 @@ class TimeEntryTest < Minitest::Test
     sql = "SELECT COUNT(client_id) FROM projects GROUP BY client_id"
     assert_equal [[3], [3], [3], [3], [6], [3], [3], [3], [3]], db.execute(sql)
   end
+
+  def test_time_entries_with_client_name
+    db = SQLite3::Database.new "time_entries.sqlite3"
+    sql = "SELECT clients.name, time_entries.id, developer_id, project_id, worked_on, duration, time_entries.created_at, time_entries.updated_at FROM time_entries INNER JOIN projects ON time_entries.project_id = projects.id INNER JOIN clients ON projects.client_id = clients.id"
+    assert_equal ["Goodwin Group", 500, 50, 30, "2014-09-22", 4, "2015-07-14 16:15:19.621743", "2015-07-14 16:15:19.621743"], db.execute(sql).last
+  end
 end
